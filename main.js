24fe1180c7b578f3b5641314c0c556346a99d26b
Woyengikuro Subiri <wsubiri@gmail.com>
2025-01-30 10:33:39 +0100

    Create library, add books and display them

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function books(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages
-    this.readBook = readBook;
+    // this.readBook = readBook;
     // this.report = function() {
     //     const readStatus = this.hasRead ? "already read" : "not read yet";
     //     return'${this.title} by ${this.author}, ${this.pages}, pages ${this.readBook}.';
@@ -13,6 +13,20 @@ function books(title, author, pages) {
 
 function addBookToLibrary (title, author, pages) {
     //take params, create a book then store it in the array
-    const firstBook = new books("Taming of the Shrew", "Willaim Shakespear", 230);
-    myLibrary.push(firstBook);
+    let newBook = new books(title, author, pages);
+    myLibrary.push(newBook);
 };
+
+let indexOfList = 0;
+
+addBookToLibrary("Taming of the Shrew", "William Shakespear", 230);
+addBookToLibrary("Things Fall Apart", "Chinua Achebe", 209);
+addBookToLibrary("Half of a Yellow Sun", "Chimamanda Ngozi Adichie", 433);
+
+function displayBooks (){
+    myLibrary.forEach((books, index) => {
+        console.log(`${indexOfList += 1}. Title: ${books.title}, Author: ${books.author}, Number of Pages: ${books.pages}.`);
+    });
+}
+
+displayBooks();
